ca13a349929f1c6f0b466e810a7302f7d4f9021f
zhengshiqi <zhengshiqi@hd123.com>
2018-12-21 16:43:17 +0800

    实现serializable接口

--- DEMO API/src/main/java/com/hd123/demo/service/base/Test.java
+++ DEMO API/src/main/java/com/hd123/demo/service/base/Test.java
@@ -1,5 +1,9 @@
 package com.hd123.demo.service.base;
 
-public class Test {
+import java.io.Serializable;
+
+public class Test implements Serializable {
+
+  private static final long serialVersionUID = -4575729513448309469L;
 
 }
