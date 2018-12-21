614175033d1c1cba76d7d4b79d9cff2174ae4563
zhengshiqi <zhengshiqi@hd123.com>
2018-12-21 16:44:11 +0800

    uuid属性

--- DEMO API/src/main/java/com/hd123/demo/service/base/Test.java
+++ DEMO API/src/main/java/com/hd123/demo/service/base/Test.java
@@ -6,4 +6,14 @@ public class Test implements Serializable {
 
   private static final long serialVersionUID = -4575729513448309469L;
 
+  private String uuid;
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
+
 }
